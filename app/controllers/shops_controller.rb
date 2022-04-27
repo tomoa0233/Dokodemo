0d820ff953601316b3ebe837963f1e3cc073845b
tomoa0233 <tomoa0233@gmail.com>
2022-04-27 08:08:29 +0000

    session fix

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,4 +1,8 @@
 class ShopsController < ApplicationController
+  def new
+    @shop = Shop.new
+  end
+
   def index
   end
 
@@ -6,5 +10,12 @@ class ShopsController < ApplicationController
   end
 
   def create
+    @shop = Shop.new(shop_params)
+  end
+
+  private
+
+  def shop_params
+    params.require(:shop).permit(:shop_name, :address, :shop_hp, :telephone, :email, :introduction, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   patch 'users/withdraw'
   get 'users/mypage' => 'users#show'
   resources :users, only: [:edit, :update]
-  resources :shops, only: [:index, :show, :create]
+  resources :shops, only: [:new, :index, :show, :create]
   resources :comments
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
